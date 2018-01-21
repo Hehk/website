e861321c4f9545685f1bd6fe7c0e63711aafd756
hehk <kyle1101000@gmail.com>
2018-01-20 21:57:03 -0500

    added new blog post

--- src/data/writing.js
+++ src/data/writing.js
@@ -1,5 +1,10 @@
 const writing = [
   {
+    title: "Creating global state in ReasonReact",
+    message: "A short post on how to move state out of reducer components",
+    link: "https://medium.com/@Hehk/creating-global-state-in-reasonreact-f84701c6ab6"
+  },
+  {
     title: "Routing in ReasonReact",
     message: "A short post on how to use the new ReasonReact.Router module",
     link: "https://medium.com/@Hehk/routing-in-reasonreact-d512fdc81b63"
