c26ce66efe21592df145d46e31321dcec9b4bf73
hehk <kyle1101000@gmail.com>
2018-01-28 14:51:23 -0500

    added new blog post

--- src/data/writing.js
+++ src/data/writing.js
@@ -1,8 +1,14 @@
 const writing = [
   {
+    title: "Binding a library in ReasonML",
+    message: "A short post about creating a binding for the Commander library",
+    link: "https://medium.com/@Hehk/binding-a-library-in-reasonml-e33b6a58b1b3"
+  },
+  {
     title: "Creating global state in ReasonReact",
     message: "A short post on how to move state out of reducer components",
-    link: "https://medium.com/@Hehk/creating-global-state-in-reasonreact-f84701c6ab6"
+    link:
+      "https://medium.com/@Hehk/creating-global-state-in-reasonreact-f84701c6ab6"
   },
   {
     title: "Routing in ReasonReact",
