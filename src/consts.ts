22fab461c8f846d38611945ac56976dad3f0f4d7
kyle henderson <ona@lunch.run>
2026-01-06 22:15:36 -0600

    Formatting

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'I No Read';
-export const SITE_DESCRIPTION = 'striving for illiteracy';
+export const SITE_TITLE = "I No Read";
+export const SITE_DESCRIPTION = "striving for illiteracy";
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        serif: ['EB Garamond', 'Georgia', 'serif'],
-        mono: ['IBM Plex Mono', 'monospace'],
+        serif: ["EB Garamond", "Georgia", "serif"],
+        mono: ["IBM Plex Mono", "monospace"],
       },
       typography: {
         DEFAULT: {
           css: {
-            fontFamily: 'EB Garamond, Georgia, serif',
-            h1: { fontFamily: 'EB Garamond, Georgia, serif' },
-            h2: { fontFamily: 'EB Garamond, Georgia, serif' },
-            h3: { fontFamily: 'EB Garamond, Georgia, serif' },
-            h4: { fontFamily: 'EB Garamond, Georgia, serif' },
-            h5: { fontFamily: 'EB Garamond, Georgia, serif' },
-            h6: { fontFamily: 'EB Garamond, Georgia, serif' },
+            fontFamily: "EB Garamond, Georgia, serif",
+            h1: { fontFamily: "EB Garamond, Georgia, serif" },
+            h2: { fontFamily: "EB Garamond, Georgia, serif" },
+            h3: { fontFamily: "EB Garamond, Georgia, serif" },
+            h4: { fontFamily: "EB Garamond, Georgia, serif" },
+            h5: { fontFamily: "EB Garamond, Georgia, serif" },
+            h6: { fontFamily: "EB Garamond, Georgia, serif" },
           },
         },
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
